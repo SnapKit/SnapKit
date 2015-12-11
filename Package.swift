aaccbd929a0e6d63570010d732cb13d175279794
gitpark <gitpark.sunny@gmail.com>
2015-12-11 11:32:46 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+/*
+ This source file is part of the Swift.org open source project
+ Copyright 2015 Apple Inc. and the Swift project authors
+ Licensed under Apache License v2.0 with Runtime Library Exception
+ See http://swift.org/LICENSE.txt for license information
+ See http://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import PackageDescription
+
+let package = Package(
+    name: "SnapKit"
+)
