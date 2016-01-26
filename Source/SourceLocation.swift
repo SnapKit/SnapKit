105dd74a9c7a1c378c6a0f75955c5eb6c794a93b
Robert Payne <robertpayne@me.com>
2016-01-27 00:44:05 +1300

    Re-add SourceLocation.swift

--- Source/SourceLocation.swift
+++ Source/SourceLocation.swift
@@ -21,6 +21,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+
 public struct SourceLocation {
     
     public let file: String
@@ -32,5 +33,3 @@ public struct SourceLocation {
     }
     
 }
-
-
