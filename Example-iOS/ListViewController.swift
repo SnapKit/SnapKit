b206d5ffd594c9489da67569628e2245c4f77ca1
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-01 11:58:06 +0200

    adding example project

---
+++ Example-iOS/ListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Spiros Gerokostas on 01/03/16.
+//  Copyright © 2016 SnapKit Team. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "SnapKit iOS Demos"
+    }
+
+    
+
+
+}
+
