60ebb0e45bb340ce5ee6a5490fd94e1fc245da7e
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-01 13:00:56 +0200

    adding basic uiscrollview

--- Example-iOS/demos/BasicUIScrollViewController.swift
+++ Example-iOS/demos/BasicUIScrollViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class BasicUIScrollViewController: UIViewController {
 
+    var didSetupConstraints = false
+    
+    let scrollView  = UIScrollView()
+    let contentView = UIView()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .blueColor()
+        label.numberOfLines = 0
+        label.lineBreakMode = .ByClipping
+        label.textColor = .whiteColor()
+        label.text = NSLocalizedString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", comment: "")
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.whiteColor()
+        
+        view.addSubview(scrollView)
+        
+        contentView.backgroundColor = UIColor.redColor()
+        scrollView.addSubview(contentView)
+        contentView.addSubview(label)
+        
+        view.setNeedsUpdateConstraints()
+    }
+    
+    override func updateViewConstraints() {
+        
+        if (!didSetupConstraints) {
+            scrollView.snp_makeConstraints { (make) -> Void in
+                make.edges.equalTo(view).inset(UIEdgeInsetsZero)
+            }
+            
+            contentView.snp_makeConstraints { (make) -> Void in
+                make.edges.equalTo(scrollView).inset(UIEdgeInsetsZero)
+                make.width.equalTo(scrollView)
+            }
+            
+            label.snp_makeConstraints { (make) -> Void in
+                make.top.equalTo(contentView).inset(20)
+                make.leading.equalTo(contentView).inset(20)
+                make.trailing.equalTo(contentView).inset(20)
+                make.bottom.equalTo(contentView).inset(20)
+            }
+            
+            didSetupConstraints = true
+        }
+        
+        super.updateViewConstraints()
     }
 }
