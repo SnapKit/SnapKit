67ec3a60c5e4c06afd017492bd134e13b56b39b4
Meng <hzzhangmeng1@corp.netease.com>
2016-08-29 16:22:58 +0800

    fixbug: store constraint issue

--- Source/ConstraintDescription.swift
+++ Source/ConstraintDescription.swift
@@ -40,24 +40,26 @@ public class ConstraintDescription {
     internal var constant: ConstraintConstantTarget = 0.0
     internal var priority: ConstraintPriorityTarget = 1000.0
     internal var constraint: Constraint? {
-        guard let relation = self.relation,
-              let related = self.related,
-              let sourceLocation = self.sourceLocation else {
-            return nil
+        if internalConstraint == nil {
+            guard let relation = self.relation,
+                let related = self.related,
+                let sourceLocation = self.sourceLocation else {
+                    return nil
+            }
+            let from = ConstraintItem(target: self.view, attributes: self.attributes)
+            
+            internalConstraint = Constraint(from: from,
+                                            to: related,
+                                            relation: relation,
+                                            sourceLocation: sourceLocation,
+                                            label: self.label,
+                                            multiplier: self.multiplier,
+                                            constant: self.constant,
+                                            priority: self.priority)
         }
-        let from = ConstraintItem(target: self.view, attributes: self.attributes)
-        
-        return Constraint(
-            from: from,
-            to: related,
-            relation: relation,
-            sourceLocation: sourceLocation,
-            label: self.label,
-            multiplier: self.multiplier,
-            constant: self.constant,
-            priority: self.priority
-        )
+        return internalConstraint
     }
+    private var internalConstraint: Constraint?
     
     // MARK: Initialization
     
