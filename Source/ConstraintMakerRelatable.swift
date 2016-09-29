8daccc8d60dd0ca59b4f7ef8f39e4742ca769eb9
Txai Wieser <txaidw@gmail.com>
2016-09-29 17:06:27 -0300

    Add ConstraintMaker shortcuts for superview access
    Added two new shortcuts for access superview in ConstraintMakerRelatable
    `greaterThanOrEqualToSuperview`
    `lessThanOrEqualToSuperview`

--- Source/ConstraintMakerRelatable.swift
+++ Source/ConstraintMakerRelatable.swift
@@ -88,8 +88,23 @@ public class ConstraintMakerRelatable {
     }
     
     @discardableResult
+    public func lessThanOrEqualToSuperview(_ file: String = #file, _ line: UInt = #line) -> ConstraintMakerEditable {
+        guard let other = self.description.view.superview else {
+            fatalError("Expected superview but found nil when attempting make constraint `lessThanOrEqualToSuperview`.")
+        }
+        return self.relatedTo(other, relation: .lessThanOrEqual, file: file, line: line)
+    }
+    
+    @discardableResult
     public func greaterThanOrEqualTo(_ other: ConstraintRelatableTarget, _ file: String = #file, line: UInt = #line) -> ConstraintMakerEditable {
         return self.relatedTo(other, relation: .greaterThanOrEqual, file: file, line: line)
     }
     
+    @discardableResult
+    public func greaterThanOrEqualToSuperview(_ file: String = #file, line: UInt = #line) -> ConstraintMakerEditable {
+        guard let other = self.description.view.superview else {
+            fatalError("Expected superview but found nil when attempting make constraint `greaterThanOrEqualToSuperview`.")
+        }
+        return self.relatedTo(other, relation: .greaterThanOrEqual, file: file, line: line)
+    }
 }
