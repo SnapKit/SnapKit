6d4e42373458ee0c29ea4ad5db02c0a4845db4cd
Michael Buxton <michael_buxton@hotmail.com>
2016-11-26 22:27:23 +0000

    Update example project for Swift 3

--- Example-iOS/ListViewController.swift
+++ Example-iOS/ListViewController.swift
@@ -13,35 +13,34 @@ class ListViewController: UITableViewController {
 
     let kCellIdentifier = "CellIdentifier"
     let demos = ["Simple Layout", "Basic UIScrollView"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "SnapKit iOS Demos"
         
-        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: kCellIdentifier)
+        self.tableView?.register(UITableViewCell.self, forCellReuseIdentifier: kCellIdentifier)
     }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier)! as UITableViewCell
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return demos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCellIdentifier)! as UITableViewCell
         
         cell.textLabel?.text = demos[indexPath.row]
         
         return cell
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return demos.count
-    }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             let viewController = SimpleLayoutViewController()
             navigationController?.pushViewController(viewController, animated: true)
         } else if indexPath.row == 1 {
-            let viewController = BasicUIScrollViewController()
+            let viewController = ViewController()
             navigationController?.pushViewController(viewController, animated: true)
         }
     }
 }
-
