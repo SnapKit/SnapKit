63bf462e8107872e3c51e9c5400670e591072018
Felix Lapalme <lapfelix@users.noreply.github.com>
2016-12-25 06:37:38 -0500

    Make ConstraintMakerRelatable targets work with UILayoutGuide (#372)
    * Make ConstraintMakerRelatable targets work with UILayoutGuide
    
    * Fix unit tests on macOS
    
    * Fix the OS API version availability directives

--- Source/ConstraintMakerRelatable.swift
+++ Source/ConstraintMakerRelatable.swift
@@ -57,6 +57,9 @@ public class ConstraintMakerRelatable {
         } else if let other = other as? ConstraintConstantTarget {
             related = ConstraintItem(target: nil, attributes: ConstraintAttributes.none)
             constant = other
+        } else if #available(iOS 9.0, OSX 10.11, *), let other = other as? ConstraintLayoutGuide {
+            related = ConstraintItem(target: other, attributes: ConstraintAttributes.none)
+            constant = 0.0
         } else {
             fatalError("Invalid constraint. (\(file), \(line))")
         }
