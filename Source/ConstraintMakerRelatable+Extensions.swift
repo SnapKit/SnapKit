fa03c33a2342a139d3f2c716e7d317e0311ba9ae
Jimi <jimi-hpcnt@hpcnt.com>
2020-04-29 17:11:35 +0900

    fix fatalError message

--- Source/ConstraintMakerRelatable+Extensions.swift
+++ Source/ConstraintMakerRelatable+Extensions.swift
@@ -33,7 +33,7 @@ extension ConstraintMakerRelatable {
     @discardableResult
     public func equalToSuperview<T: ConstraintRelatableTarget>(_ closure: (ConstraintView) -> T, _ file: String = #file, line: UInt = #line) -> ConstraintMakerEditable {
         guard let other = self.description.item.superview else {
-            fatalError("Expected superview but found nil when attempting make constraint `greaterThanOrEqualToSuperview`.")
+            fatalError("Expected superview but found nil when attempting make constraint `equalToSuperview`.")
         }
         return self.relatedTo(closure(other), relation: .equal, file: file, line: line)
     }
@@ -41,7 +41,7 @@ extension ConstraintMakerRelatable {
     @discardableResult
     public func lessThanOrEqualToSuperview<T: ConstraintRelatableTarget>(_ closure: (ConstraintView) -> T, _ file: String = #file, line: UInt = #line) -> ConstraintMakerEditable {
         guard let other = self.description.item.superview else {
-            fatalError("Expected superview but found nil when attempting make constraint `greaterThanOrEqualToSuperview`.")
+            fatalError("Expected superview but found nil when attempting make constraint `lessThanOrEqualToSuperview`.")
         }
         return self.relatedTo(closure(other), relation: .lessThanOrEqual, file: file, line: line)
     }
